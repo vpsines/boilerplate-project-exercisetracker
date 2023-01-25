056b1a601476f74901d90872373127c8e9902ebc
vpsines <sasip6028@gmail.com>
2023-01-25 21:06:41 +0530

    feat: get /logs with req fields

--- index.js
+++ index.js
@@ -100,7 +100,44 @@ app.post("/api/users/:_id/exercises", async function (req, res) {
 });
 
 // get excercise log of users
-app.get("/api/users/:_id/logs", function (req, res) {});
+app.get("/api/users/:_id/logs", async function (req, res) {
+  var fromDate = req.query.from;
+  var toDate = req.query.to;
+  var limit = parseInt(req.query.limit);
+  //var user = await User.findOne({_id:req.params._id},{exercises:{$slice: limit}}).populate("-exercises._id");
+  var users = await User.aggregate([
+    {
+      $match: { _id: mongoose.Types.ObjectId(req.params._id) },
+    },
+
+    {
+      $project: {
+        _id: 1,
+        username: 1,
+        "exercises.description": 1,
+        "exercises.duration": 1,
+        "exercises.date": 1,
+      },
+    },
+    {
+      $project: {
+        "exercises":{$slice : ["$exercises",0,limit]}
+      },
+    },
+  ]).exec();
+  try {
+    let user = users[0];
+    var result = {
+      username: user.username,
+      count: user.exercises.length,
+      _id: user._id,
+      log: user.exercises,
+    };
+    res.json(result);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
