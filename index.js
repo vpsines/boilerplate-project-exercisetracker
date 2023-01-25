5d9e15ed0ab1f618cd5d478bf869c813decb4427
vpsines <sasip6028@gmail.com>
2023-01-25 12:08:52 +0530

    feat: add 'api/users' get and post route

--- index.js
+++ index.js
@@ -1,63 +1,81 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // get mongo url
 let mongoUri = process.env.MONGO_URI;
 
 // connect to database
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true },(err)=>{
-  if(!err) {
-    console.log("Connected...");
-  }else{
-    console.error(err);
+mongoose.connect(
+  mongoUri,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (!err) {
+      console.log("Connected...");
+    } else {
+      console.error(err);
+    }
   }
-});
+);
 
 // user schema
 const userSchema = mongoose.Schema({
-  username:String
-});
+  username: String,
+},
+{versionKey:false});
 
 // exercise schema
 const exerciseSchema = mongoose.Schema({
   username: String,
   description: String,
   duration: Number,
-  date: Date
-});
+  date: Date,
+},{versionKey:false});
 
-var User = mongoose.model('User',userSchema);
-var Exercise = mongoose.model('Exercise',exerciseSchema);
+var User = mongoose.model("User", userSchema);
+var Exercise = mongoose.model("Exercise", exerciseSchema);
 
 // add user
-app.post('/api/users',function(req,res){
-
+app.post("/api/users", async function (req, res) {
+  var user = new User(req.body);
+  try {
+    await user.save();
+    res.json(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 // get users
-app.get('/api/users',function(req,res){
+app.get("/api/users", async function (req, res) {
+  var users =await User.find({});
 
+  try {
+     res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // add excercises of user
-app.post("/api/users/:_id/exercises",function(req,res){
-
-});
+app.post("/api/users/:_id/exercises", function (req, res) {});
 
 // get excercise log of users
-app.get("/api/users/:_id/logs",function(req,res){
-
-});
+app.get("/api/users/:_id/logs", function (req, res) {});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
